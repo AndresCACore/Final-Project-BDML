ccd70ead7c497e3654740c2b4f34ab4f730f3f06
andresca <andresca@LAPTOP-SIN7GJN1.localdomain>
2022-03-09 23:52:57 -0500

    Last Update

---
+++ face_recognition_PyQt/mainwindow.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog
+from .out_window import Ui_OutputDialog
+
+class Ui_Dialog(QDialog):
+    def __init__(self):
+        super(Ui_Dialog, self).__init__()
+        self._new_window = Ui_OutputDialog()
+        self._new_window.show()
+        self._new_window.startVideo("0")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = Ui_Dialog()
+    sys.exit(app.exec_())

51780dac578fb8474e9e008cc8bd2bafd4bb68b5
andresca <andresca@LAPTOP-SIN7GJN1.localdomain>
2022-03-11 00:35:04 -0500

    Last Update

---
+++ face_recognition_PyQt/face_recog.py
@@ -0,0 +1,19 @@
+import cv2
+from recorder import recorder
+import datetime
+
+def face_recog(faces, auxFrame, frame, drowsy_recognizer, imagePaths):
+    for (x,y,w,h) in faces:
+        rostro = auxFrame[y:y+h,x:x+w]
+        rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
+        result = drowsy_recognizer.predict(rostro)
+        cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+                        
+        if result[1] < 70:
+            cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+            date_time_string = datetime.datetime.now().strftime("%y/%m/%d %H:%M:%S")
+            recorder(date_time_string, imagePaths[result[0]])
+        else:
+            cv2.putText(frame,'No identificado',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
--- face_recognition_PyQt/out_window.py
+++ face_recognition_PyQt/out_window.py
@@ -4,7 +4,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import cv2
 import datetime
-from recorder import recorder
+from face_recog import face_recog
 import sys
 
 class Ui_OutputDialog(QDialog):
@@ -60,12 +60,12 @@ class Worker1(QThread):
         print('imagePaths=',imagePaths)
             #predict
 
-            #Capture = cv2.VideoCapture(0) #original
+            
         Capture = cv2.VideoCapture(0,cv2.CAP_DSHOW) #predict
 
-            #precit
+            #predict
         faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-            #precit
+            #predict
 
         while self.ThreadActive: 
             ret, frame = Capture.read()
@@ -77,26 +77,8 @@ class Worker1(QThread):
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 auxFrame = gray.copy()
                 faces = faceClassif.detectMultiScale(gray,1.3,5)
+                face_recog(faces, auxFrame, frame, drowsy_recognizer, imagePaths)
 
-                for (x,y,w,h) in faces:
-                    rostro = auxFrame[y:y+h,x:x+w]
-                    rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
-                    result = drowsy_recognizer.predict(rostro)
-                    cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-                     
-                    if result[1] < 70:
-                        cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-                        cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-                        #self.StatusLabel.setText(imagePaths[result[0]])
-                        date_time_string = datetime.datetime.now().strftime("%y/%m/%d %H:%M:%S")
-                        recorder(date_time_string, imagePaths[result[0]])
-
-                    else:
-                        cv2.putText(frame,'No identificado',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-                        cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-                    #predict
-
-                    #FlippedImage = cv2.flip(frame, 1)
                 FlippedImage = frame
                 ConvertToQtFormat = QImage(FlippedImage, FlippedImage.shape[1], FlippedImage.shape[0], QImage.Format_RGB888)
                 Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
